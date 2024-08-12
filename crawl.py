ad9c7e8c688719a116673f8973aaa82ce151d900
Atiq Mansoori <er_asm@outlook.com>
2024-08-12 12:20:22 +0530

    Final results stored

--- crawl.py
+++ crawl.py
@@ -25,23 +25,23 @@ import os
 
 # Request the page
 
-url_id='second'
-url='https://insights.blackcoffer.com/streamlined-integration-interactive-brokers-api-with-python-for-desktop-trading-application/'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+# url_id='second'
+# url='https://insights.blackcoffer.com/streamlined-integration-interactive-brokers-api-with-python-for-desktop-trading-application/'
+# response = requests.get(url)
+# soup = BeautifulSoup(response.content, 'html.parser')
 
-# Extract title and article text
-title = soup.find('h1').get_text()
-paragraphs = soup.find_all('p')
-article_text = " ".join([p.get_text() for p in paragraphs])
+# # Extract title and article text
+# title = soup.find('h1').get_text()
+# paragraphs = soup.find_all('p')
+# article_text = " ".join([p.get_text() for p in paragraphs])
 
-## All the articles have same content after wors "Summarized"
+# ## All the articles have same content after wors "Summarized"
 
-position = article_text.find("Summarized")
+# position = article_text.find("Summarized")
 
-if position != -1:
-    article_text=article_text[:position + len("Summarized")]
+# if position != -1:
+#     article_text=article_text[:position + len("Summarized")]
 
-# Save to a text file
-with open(f'{url_id}.txt', 'w', encoding='utf-8') as file:
-    file.write(title + "\n" + article_text)
+# # Save to a text file
+# with open(f'{url_id}.txt', 'w', encoding='utf-8') as file:
+#     file.write(title + "\n" + article_text)
